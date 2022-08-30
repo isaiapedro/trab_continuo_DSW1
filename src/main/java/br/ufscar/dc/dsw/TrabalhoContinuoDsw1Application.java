5133c584df7232872cf08d3ea374435e70ca05ae
Pedro Isaia <isaiacontato@gmail.com>
2022-08-29 22:32:28 -0300

    base do SpringMVC

---
+++ src/main/java/br/ufscar/dc/dsw/TrabalhoContinuoDsw1Application.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoContinuoDsw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoContinuoDsw1Application.class, args);
+	}
+
+}
